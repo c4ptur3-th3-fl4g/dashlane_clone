fa36cc6477eb55244261ed5d557fae02150d7e59
c4ptur3-th3-fl4g <22010222@st.phenikaa-uni.edu.vn>
2025-02-25 17:50:34 +0700

    Create Dashboard page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ForgotPasswordController;
+use Illuminate\Support\Collection;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
